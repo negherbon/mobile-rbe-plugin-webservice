edf4e270ab89b1147ac6cebb4ab24306f4da637d
Rafael Negherbon <rafael.negherbon@gmail.com>
2014-03-25 13:56:08 -0300

    Inserido o plugin no objeto window

--- www/webservice.js
+++ www/webservice.js
@@ -1,27 +1,30 @@
-var RBEWebservice = {
-    callWebservice: function(){
-        // So chama se houver comunicacao
-        if (RBEWebservice.hasConnection){
-            return cordova.exec(RBEWebservice.webserviceSuccess, RBEWebservice.webserviceFail, "RBEWebservice", "webservice", []);
-        }
-    },
-    hasConnection: function(){
-        var networkState = navigator.network.connection.type;
+window.RBEWebservice = function (str, callback){
     
-        // Se tiver conexao
-        if  (networkState!=Connection.NONE && networkState==Connection.UNKNOWN){
-            return true;
-        }
-        
-        return false;
-    },
-    webserviceSuccess: function(){
-        
-    },
-    webserviceFail: function(){
+    return cordova.exec(callback, function(err) { callback('Nothing to echo.');
+                        }, "RBEWebservice", "webservice", [str]);
     
-    }
+    /*
+     callWebservice: function(){
+     // So chama se houver comunicacao
+     if (RBEWebservice.hasConnection){
+     
+     }
+     },
+     hasConnection: function(){
+     var networkState = navigator.network.connection.type;
+     
+     // Se tiver conexao
+     if  (networkState!=Connection.NONE && networkState==Connection.UNKNOWN){
+     return true;
+     }
+     
+     return false;
+     },
+     webserviceSuccess: function(){
+     
+     },
+     webserviceFail: function(){
+     
+     }*/
+    
 };
-
-module.exports = RBEWebservice;
-
